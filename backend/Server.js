a62f89551d3a61a47d5b30f13d0645b06bb6fcf8
sanjibchowdhury <sanjibchowdhury556@gmail.com>
2025-08-19 19:46:08 +0530

    second commit

--- backend/Server.js
+++ backend/Server.js
@@ -19,7 +19,10 @@ app.use('/api/elections', electionRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/votes', voteRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}/`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}/`);
+// });
+
+module.exports = app;
+
